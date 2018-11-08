48897c3bb80c73ef1c560ee6c9450ea9a2278453
Unknown <carfi.alessandro@gmail.com>
2018-11-08 08:19:20 +0100

    comments update

--- src/gb_controller.py
+++ src/gb_controller.py
@@ -35,6 +35,8 @@ class GestureController(object):
         self.pub_mode = rospy.Publisher('/cmd_mode', UInt16, queue_size=1)
         rospy.Subscriber('/inertial', Imu, self.callback_continuos_control)
 
+    ##  update_rate Node frequency (Hz)
+    self.update_rate = 10  
 
     def callback_continuos_control(self, data):
         """Callback manager"""
