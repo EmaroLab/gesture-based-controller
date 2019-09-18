a2822326a5ef7d42a6bab3f2a443d81c77529fd5
BolleA7X <alessio.bollea@live.it>
2019-09-18 15:36:24 +0200

    Implementation of nonlinear mapping

--- src/gb_controller.py
+++ src/gb_controller.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 import rospy
+import sys
+from numpy import sign
 from geometry_msgs.msg import Pose, Twist
 from sensor_msgs.msg import Imu
 from std_msgs.msg import UInt16
@@ -16,6 +18,10 @@ class GestureController(object):
 
         ## Flag for the method to apply (nonlinear: 1, linear: any other value)
         self.nonlinear_flag = rospy.get_param('nonlinear', 0)
+        if self.nonlinear_flag == 1:
+            rospy.loginfo("USING NONLINEAR MAPPING")
+        else:
+            rospy.loginfo("USING LINEAR MAPPING")
 
         ## Mapping coefficient for linear velocity
         self.linear_coefficent = rospy.get_param ('linear_coefficient', 0.05)
@@ -24,6 +30,28 @@ class GestureController(object):
 
         ## Halt threshold: if the linear velocity is less than this value, the mower doesn't move
         self.halt_threshold = rospy.get_param ('halt_threshold', 0)
+
+        ## Saturation values for x and y acceleration from the smartwatch
+        self.x_acc_saturation = rospy.get_param ('x_acceleration_saturation', 8)
+        self.y_acc_saturation = rospy.get_param ('y_acceleration_saturation', 6)
+
+        ## The two saturation and the threshold values must be positive.
+        #  The halt threshold must be less than both values.
+        #  This check if performed only if the nonlinear flag is high
+        if self.nonlinear_flag == 1 and ( \
+           self.halt_threshold <= 0 or \
+           self.x_acc_saturation <= 0 or \
+           self.y_acc_saturation <= 0 or \
+           self.halt_threshold >= self.x_acc_saturation or \
+           self.halt_threshold >= self.y_acc_saturation):
+            rospy.logfatal("INVALID LAUNCHFILE PARAMETERS VALUES!")
+            sys.exit(1)
+
+        ## If the nonlinear flag is high, the two mapping coefficients are updated
+        #  0.4 and 0.3 are taken as the maximum linear and angular velocities possible
+        if self.nonlinear_flag == 1:
+            self.linear_coefficent = 0.4 / (self.x_acc_saturation - self.halt_threshold)
+            self.angular_coefficent = 0.3 / (self.y_acc_saturation - self.halt_threshold)
 
         ## Stores the last acceleration received by the node
         self.last_acc = [0,0,0]
@@ -50,7 +78,7 @@ class GestureController(object):
     #
     #  @param self The object pointer
     def run(self):
-        self.init()
+        #self.init()
         r = rospy.Rate(self.update_rate)
         while True:
             try:
@@ -86,13 +114,25 @@ class GestureController(object):
     #
     #  @param self The object pointer
     def v_nonlinear_mapping(self):
-        pass
+        x_acc = self.last_acc[0]
+        if abs(x_acc) < self.halt_threshold:
+            return 0
+        elif abs(x_acc) >= self.x_acc_saturation:
+            return 0.4 * sign(x_acc)
+        else:
+            return self.linear_coefficent * (x_acc - self.halt_threshold * sign(x_acc))
 
     ## Nonlinear mapping function for the angular velocity
     #
     #  @param self The object pointer
     def w_nonlinear_mapping(self):
-        pass
+        y_acc = self.last_acc[1]
+        if abs(y_acc) < self.halt_threshold:
+            return 0
+        elif abs(y_acc) >= self.y_acc_saturation:
+            return 0.3 * sign(y_acc)
+        else:
+            return self.angular_coefficent * (y_acc - self.halt_threshold * sign(y_acc))
 
     ## Mapping of acceleration to robot angular and linear velocities
     #
@@ -109,7 +149,6 @@ class GestureController(object):
         else:
             twist.linear.x = self.v_nonlinear_mapping()
             twist.angular.z = self.w_nonlinear_mapping()
-            pass
         self.pub_twist.publish(twist)
 
 
